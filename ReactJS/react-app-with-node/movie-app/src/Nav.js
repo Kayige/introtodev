bced7fabba4d52fba176eb6b8bb84ad0d28264d8
Gabriel Lee <gabrielleeyj@outlook.com>
2020-11-03 16:03:25 +0800

    update with pages

--- ReactJS/react-app-with-node/movie-app/src/Nav.js
+++ ReactJS/react-app-with-node/movie-app/src/Nav.js
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from "react";
 import "./Nav.css";
+import { Link } from "react-router-dom";
 
 function Nav() {
   const [show, handleShow] = useState(false);
+
+  const navStyle = {
+    color: "white",
+  };
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
@@ -22,6 +27,14 @@ function Nav() {
         src="https://i.pinimg.com/originals/9b/3f/9c/9b3f9c24d9fd5f297ae433eb33d93514.png"
         alt="Logo"
       />
+      <ul className="nav_links">
+        <Link style={navStyle} to="/categories">
+          <li>Categories</li>
+        </Link>
+        <Link style={navStyle} to="/search">
+          <li>Search</li>
+        </Link>
+      </ul>
     </div>
   );
 }
