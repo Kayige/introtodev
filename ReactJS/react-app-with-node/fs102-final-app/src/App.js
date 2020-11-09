b25a2edc7663be9d35b49774a24f1dd0bd85fe1f
Gabriel Lee <gabrielleeyj@outlook.com>
2020-11-09 19:57:27 +0800

    Add Header & Banner

--- ReactJS/react-app-with-node/fs102-final-app/src/App.js
+++ ReactJS/react-app-with-node/fs102-final-app/src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import React from "react";
+import Header from "./components/Header";
+import Banner from "./components/Banner";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
+        <Banner />
         <Switch>
           <Route path="/" exact></Route>
         </Switch>
