1ea8888f5dd58d7f2296e4f3fe6b8651712d4077
Gabriel Lee <gabrielleeyj@outlook.com>
2020-11-09 19:10:18 +0800

    initial commit for final project

---
+++ ReactJS/react-app-with-node/fs102-final-app/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/" exact></Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
