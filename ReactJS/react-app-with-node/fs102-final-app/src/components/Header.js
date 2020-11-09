a3b55c77057fa8b6515042602700ce69a0c9d0c2
Gabriel Lee <gabrielleeyj@outlook.com>
2020-11-09 20:39:57 +0800

    changed styling of header search

--- ReactJS/react-app-with-node/fs102-final-app/src/components/Header.js
+++ ReactJS/react-app-with-node/fs102-final-app/src/components/Header.js
@@ -33,6 +33,16 @@ export const Header = () => {
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
           <Button variant="outline-success">Search</Button>
         </Form>
+        {/* <InputGroup className="mb-3">
+    <FormControl
+      placeholder="Recipient's username"
+      aria-label="Recipient's username"
+      aria-describedby="basic-addon2"
+    />
+    <InputGroup.Append>
+      <Button variant="outline-secondary">Button</Button>
+    </InputGroup.Append>
+  </InputGroup> */}
       </Navbar.Collapse>
     </Navbar>
   );
