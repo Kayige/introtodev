60945bb8ef81ac505529380645c2edb35c056629
Gabriel Lee <gabrielleeyj@outlook.com>
2020-11-09 22:00:00 +0800

    imported collections

--- ReactJS/react-app-with-node/fs102-final-app/src/App.js
+++ ReactJS/react-app-with-node/fs102-final-app/src/App.js
@@ -2,7 +2,9 @@ import "./App.css";
 import React from "react";
 import Header from "./components/Header";
 import Banner from "./components/Banner";
+import Collections from "./components/Collections";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./lib/font-awesome/css/all.min.css";
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
           <Route path="/" exact></Route>
           {/* <Router path="/"></Router> */}
         </Switch>
+        <Collections />
       </Router>
     </div>
   );
