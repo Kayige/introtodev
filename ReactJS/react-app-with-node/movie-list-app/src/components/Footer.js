528860210c969c0d856f0007fe8d95a87c3cb590
Gabriel <52558343+gabrielleeyj@users.noreply.github.com>
2021-03-12 18:48:48 +0800

    Delete ReactJS/react-app-with-node/movie-list-app directory

--- ReactJS/react-app-with-node/movie-list-app/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function Footer() {
-  return (
-    <div className="Footer">
-      <h3>Built by Gabriel Lee. Built with ReactJS. Copyright 2020.</h3>
-    </div>
-  );
-}
--- ReactJS/react-app-with-node/movie-list-app/src/components/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-// create base URL to make the requests to the database
-const instance = axios.create({
-  baseURL: "https://api.themoviedb.org/3",
-});
-
-export default instance;
